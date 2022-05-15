746707fc12a6706dd1a4538b00b9150ca2e4dac9
Pavel Kopytov <nobless2@gmail.com>
2022-05-15 12:30:39 +0300

    first

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+use Framework\Http\Request;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+$request = new Request();
+
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+
+echo 'Hello ' . $name . ' !';
